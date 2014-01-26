f165ae13b2032516f87a52c873ba74959a77fbb9
yoshinari.yuto <yoshinari.yuto@gmail.com>
2014-01-26 18:20:21 +0900

    EnemyPieceを追加

---
+++ Classes/EnemyPiece.h
@@ -0,0 +1,29 @@
+//
+//  EnemyPiece.h
+//  teamB
+//
+//  Created by cocos2d-x on 2014/01/26.
+//
+//
+
+#ifndef __teamB__EnemyPiece__
+#define __teamB__EnemyPiece__
+
+#include "cocos2d.h"
+#include "MapPiece.h"
+
+USING_NS_CC;
+
+class EnemyPiece : public MapPiece {
+    Variables::PIECE_TYPE  pieceType;
+    Variables::PIECE_IMAGE pieceImage;
+    bool isAlive;
+    
+    const char* getPszFileName(Variables::PIECE_IMAGE image);
+public:
+    static EnemyPiece* create(Variables::PIECE_IMAGE image);
+    EnemyPiece();
+    void die();
+};
+
+#endif /* defined(__teamB__EnemyPiece__) */
