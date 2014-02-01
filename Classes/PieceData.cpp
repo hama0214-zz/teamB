6cf2ffe1e71242e3da93c4b157d912f83b31a61e
Kenji Kawanabe <flankids.sakura37@gmail.com>
2014-02-02 08:06:24 +0900

    構造体PieceDataをクラスにしました。

---
+++ Classes/PieceData.cpp
@@ -0,0 +1,66 @@
+//
+//  PieceData.cpp
+//  teamB
+//
+//  Created by Kenji on 14/02/02.
+//
+//
+
+#include "PieceData.h"
+
+PieceData* PieceData::create(int dataValue) {
+    PieceData *ret = new PieceData();
+    if (ret != NULL && ret->init(dataValue)) {
+        ret->autorelease();
+        return ret;
+    }
+    
+    CC_SAFE_DELETE(ret);
+    return NULL;
+}
+
+bool PieceData::init(int dataValue) {
+    // 上2桁をピースタイプとして扱う
+    m_pieceType = convertToPieceType(dataValue / 100);
+    
+    // 下2桁を画像タイプとして扱う
+    m_imageType = convertToImageType(dataValue % 100);
+    
+    return true;
+}
+
+Variables::PIECE_TYPE PieceData::convertToPieceType(int pieceTypeValue) {
+    switch (pieceTypeValue) {
+        case 0:
+            // TODO: 空白のマスオブジェクトタイプを返す。現状フィールドタイプを返している。
+            return Variables::FIELD_PIECE;
+        case 1:
+            return Variables::FIELD_PIECE;
+        case 2:
+            return Variables::ITEM_PIECE;
+        default:
+            CCAssert(false, "不正な数を指定された。");
+            break;
+    }
+}
+
+Variables::PIECE_IMAGE PieceData::convertToImageType(int imageTypeValue) {
+    switch (imageTypeValue) {
+        case 0:
+            // TODO: 空白の画像タイプを返す。現状フィールド画像タイプを返している。
+            return Variables::FIELD_PIECE_IMAGE_0;
+        case 1:
+            return Variables::FIELD_PIECE_IMAGE_1;
+        case 2:
+            return Variables::FIELD_PIECE_IMAGE_2;
+        case 3:
+            return Variables::ITEM_PIECE_IMAGE_0;
+        case 4:
+            return Variables::ITEM_PIECE_IMAGE_1;
+        case 5:
+            return Variables::ITEM_PIECE_IMAGE_2;
+        default:
+            CCAssert(false, "不正な数を指定された。");
+            break;
+    }
+}
---
+++ Classes/PieceData.h
@@ -0,0 +1,69 @@
+//
+//  PieceData.h
+//  teamB
+//
+//  Created by Kenji on 14/02/02.
+//
+//
+
+#ifndef __teamB__PieceData__
+#define __teamB__PieceData__
+
+#include "cocos2d.h"
+#include "Variables.h"
+
+USING_NS_CC;
+
+/** 1マスのオブジェクトの生成に必要な情報 */
+class PieceData : CCObject
+{
+public:
+    /**
+     * 最大4桁の数からピースオブジェクトの生成に必要な情報を生成する。
+     * @param dataValue 4桁の数。
+     * @return ピースオブジェクトの生成に必要な情報。
+     */
+    static PieceData* create(int dataValue);
+    
+    /**
+     * ピースタイプを取得する。
+     * @return ピースタイプ。
+     */
+    inline Variables::PIECE_TYPE getPieceType() const {return m_pieceType;}
+    
+    /**
+     * 画像タイプを取得する。
+     * @return 画像タイプ。
+     */
+    inline Variables::PIECE_IMAGE getImageType() const {return m_imageType;}
+    
+private:
+    /** ピースタイプ */
+    Variables::PIECE_TYPE m_pieceType;
+    
+    /** 画像のタイプ */
+    Variables::PIECE_IMAGE m_imageType;
+    
+    /**
+     * 初期化する。
+     * @param dataValue 4桁の数。
+     * @return 初期化に成功したかどうか。
+     */
+    bool init(int dataValue);
+    
+    /**
+     * 2桁の数字からピースタイプに変換する。
+     * @param pieceTypeValue ピースタイプに変換する数字。
+     * @return ピースタイプ。
+     */
+    Variables::PIECE_TYPE convertToPieceType(int pieceTypeValue);
+    
+    /**
+     * 2桁の数字から画像タイプに変換する。
+     * @param pieceTypeValue 画像タイプに変換する数字。
+     * @return 画像タイプ。
+     */
+    Variables::PIECE_IMAGE convertToImageType(int imageTypeValue);
+};
+
+#endif /* defined(__teamB__PieceData__) */
