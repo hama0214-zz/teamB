70b462ac2fb2f7eee57e8e51b005ad1dc00dc0ee
Kenji Kawanabe <flankids.sakura37@gmail.com>
2014-02-02 02:18:45 +0900

    アスペクト比がどの端末でも同じになるようにしました。

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,5 +1,6 @@
 #include "AppDelegate.h"
 #include "MenuScene.h"
+#include "GameScene.h"
 
 USING_NS_CC;
 
@@ -21,11 +22,13 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // turn on display FPS
     pDirector->setDisplayStats(false);
 
+    pEGLView->setDesignResolutionSize(1024, 768, kResolutionShowAll);
+    
     // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    CCScene *pScene = MenuScene::scene();
+    CCScene *pScene = GameScene::scene();
 
     // run
     pDirector->runWithScene(pScene);
